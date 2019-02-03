04dd729b653c5c4509415a3cdf11d96c80065c0f
Richard Schneider <richard@schneiderbox.net>
2019-02-03 15:39:23 -0600

    remove code for non-mcts from ai module (just so it doesn't need to be kept updated with changes while I'm not using it)

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -1,11 +1,11 @@
-mod mc;
-pub use mc::mc;
+//mod mc;
+//pub use mc::mc;
 
-mod minimax;
-pub use minimax::minimax;
+//mod minimax;
+//pub use minimax::minimax;
 
 mod mcts;
 pub use mcts::mcts;
 
-mod random;
-pub use random::random;
+//mod random;
+//pub use random::random;
