85169e49a5400279f63679c386e192da8f1c6e1c
Richard Schneider <richard@schneiderbox.net>
2019-02-11 20:13:00 -0600

    formatting

--- src/ui/api.rs
+++ src/ui/api.rs
@@ -34,13 +34,13 @@ fn index(id: u64, tqbn: String) -> String {
     log.push_str(&format!("input: {}\\n", tqbn));
 
     let board = crate::Board::from_tqbn(&tqbn.to_string());
-//    log.push_str(&board.to_string());
+    //    log.push_str(&board.to_string());
 
     let child = crate::ai::default(&board, &mut log);
 
     let move_string = board.move_string_to(&child);
     log.push_str(&format!("output: {}\\n", move_string));
-//    log.push_str(&child.to_string());
+    //    log.push_str(&child.to_string());
     log = log.replace("\n", "\\n");
     log = log.replace("\t", "\\t");
 
