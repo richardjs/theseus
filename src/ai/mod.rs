c1a0c98a6f5a780db5eb0a35470eb5de773ccaf5
Richard Schneider <richard@schneiderbox.net>
2019-02-21 20:24:20 -0600

    add a presearch to make trivial moves

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -7,7 +7,26 @@
 mod mcts;
 pub use mcts::mcts;
 
+fn presearch(board: &crate::Board, log: &mut String) -> Option<crate::Board> {
+    if board.can_win() {
+        log.push_str("presearch: taking win\n");
+        return Some(board.moves_detailed(true, false, false, true)[0].clone());
+    }
+    if board.remaining_walls()[0] == 0 && board.remaining_walls()[1] == 0 {
+        log.push_str("presearch: walking shortest path\n");
+        for child in board.moves_detailed(true, false, false, true) {
+            if child.other_pawn() == *board.shortest_path(board.turn()).first().unwrap() {
+                return Some(child.clone());
+            }
+        }
+    }
+    None
+}
+
 pub fn default(board: &crate::Board, log: &mut String) -> crate::Board {
+    if let Some(board) = presearch(board, log) {
+        return board;
+    }
     mcts(board, log)
 }
 
